f7958736da486f0d479aab31939a2670c1aa2df6
mommae83 <99304302+mommae83@users.noreply.github.com>
2025-05-15 21:43:08 -0400

    Update server.js - require header removed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 cors_proxy.createServer({
   originWhitelist: [], // Allow all origins
-  requireHeader: ['origin', 'x-requested-with'],
+  requireHeader: [], // Allow all origins
   removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
   console.log(`Running CORS Anywhere on ${host}:${port}`);
